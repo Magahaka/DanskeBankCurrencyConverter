d2929fba6a6695ec09a706455a8981ffbf2a46ee
Povilas Dirsė <61330995+Magahaka@users.noreply.github.com>
2025-09-08 23:52:57 +0300

    Update InputParserHandler.cs

--- CurrencyConverter/Handlers/InputParserHandler.cs
+++ CurrencyConverter/Handlers/InputParserHandler.cs
@@ -6,6 +6,10 @@ namespace CurrencyConverter.Handlers;
 
 public class InputParserHandler(ICurrencyBroker currencyBroker) : AbstractHandler
 {
+    private const int _commandIndex = 0;
+    private const int _currencyPairIndex = 1;
+    private const int _amountIndex = 2;
+    
     public override InputContext Handle(InputContext context)
     {
         var arguments = context.UserInput
@@ -27,12 +31,12 @@ public class InputParserHandler(ICurrencyBroker currencyBroker) : AbstractHandle
     private static (string mainCurrency, string moneyCurrency, decimal amount) ConvertInputsToRequiredTypes(
         List<string> arguments)
     {
-        var currencyPair = arguments[1].Split('/');
+        var currencyPair = arguments[_currencyPairIndex].Split('/');
 
-        var mainCurrency = currencyPair[0];
-        var moneyCurrency = currencyPair[1];
+        var mainCurrency = currencyPair[_commandIndex];
+        var moneyCurrency = currencyPair[_currencyPairIndex];
 
-        var amount = decimal.Parse(arguments[2]);
+        var amount = decimal.Parse(arguments[_amountIndex]);
 
         return (mainCurrency, moneyCurrency, amount);
     }
