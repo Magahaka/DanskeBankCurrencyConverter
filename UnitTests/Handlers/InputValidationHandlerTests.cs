e81c8e01587e5d0ceeed15067b98febcde36ebb8
Povilas Dirsė <61330995+Magahaka@users.noreply.github.com>
2025-09-08 23:17:12 +0300

    Create InputValidationHandlerTests.cs

---
+++ UnitTests/Handlers/InputValidationHandlerTests.cs
@@ -0,0 +1,95 @@
+using CurrencyConverter.CurrencyProviders;
+using CurrencyConverter.Handlers;
+using CurrencyConverter.Interfaces;
+using CurrencyConverter.Models;
+using FluentAssertions;
+using NSubstitute;
+using NSubstitute.ReceivedExceptions;
+
+namespace UnitTests.Handlers;
+
+public class InputValidationHandlerTests
+{
+    private readonly ICurrencyConverterValidator _validator = Substitute.For<ICurrencyConverterValidator>();
+
+    private readonly InputValidationHandler _handler;
+    private readonly InputContext _inputContext;  
+
+    public InputValidationHandlerTests()
+    {
+        _handler = new InputValidationHandler(_validator);
+
+        _inputContext = new InputContext() { UserInput = "Exchange EUR/USD 1" };
+    }
+
+    [Fact]
+    public void Handle_ShouldCallValidators()
+    {
+        _handler.Handle(_inputContext);
+
+        _validator
+            .Received(1)
+            .ValidateInputArgumentCount("Exchange EUR/USD 1");
+
+        _validator
+            .Received(1)
+            .ValidateInputCommand("Exchange");
+
+        _validator
+            .Received(1)
+            .ValidateCurrencyPairFormat("EUR/USD");
+
+        _validator
+            .Received(1)
+            .ValidateAmount("1");
+    }
+
+    [Fact]
+    public void Handle_ShouldThrowException_WhenInputArgumentIsNotValid()
+    {
+        var exception = new Exception("error);
+
+        _validator
+            .When(x => x.ValidateInputArgumentCount(_inputContext.UserInput)
+            .Throw(exception);
+
+        _handler
+            .Invoking(x => x.Handle(_inputContext))
+            .Should()
+            .Throw<exception>()
+            .WithMessage("error");
+    }
+
+    [Fact]
+    public void Handle_ShouldThrowException_WhenCurrencyPairArgumentIdNotValid()
+    {
+        var exception = new Exception("error);
+
+        _validator
+            .When(x => x.ValidateCurrencyPairFormat(Arg.Any<string>());
+            .Throw(exception);
+
+        _handler
+            .Invoking(x => x.Handle(_inputContext))
+            .Should()
+            .Throw<exception>()
+            .WithMessage("error");
+    }
+
+    [Fact]
+    public void Handle_ShouldThrowException_WhenAmkuntArgumentIsNotValid()
+    {
+        var exception = new Exception("error);
+
+        _validator
+            .When(x => x.ValidateAmount(Arg.Any<string>());
+            .Throw(exception);
+
+        _handler
+            .Invoking(x => x.Handle(_inputContext))
+            .Should()
+            .Throw<exception>()
+            .WithMessage("error");
+    }
+}
+
